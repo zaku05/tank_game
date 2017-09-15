b2f3d06f123e3c0f46cc424624d593cbc776247f
FIVE-PC\five <0dd05yy@gmail.com>
2017-09-15 14:54:09 -0500

    160 - stable commit

--- Tank_Fite/Source/Tank_Fite/TankMovementComponent.cpp
+++ Tank_Fite/Source/Tank_Fite/TankMovementComponent.cpp
@@ -5,7 +5,6 @@
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 
@@ -13,10 +12,16 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-	// auto TrackName = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("Throttle throw: %f"), Throw)
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
-
+	// TODO prevent double speed due to dual control use
 }
 
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
+	// TODO prevent double speed due to dual control use
+}
