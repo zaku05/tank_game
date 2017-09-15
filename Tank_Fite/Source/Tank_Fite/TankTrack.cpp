e8e9e334804c36480d0da83da761ab77e0fb9a22
FIVE-PC\five <0dd05yy@gmail.com>
2017-09-15 10:28:43 -0500

    middle of 155 -- stable commit

---
+++ Tank_Fite/Source/Tank_Fite/TankTrack.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto TrackName = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s trottle: %f"), *TrackName, Throttle)
+
+	// TODO Clamp incoming throttle
+	auto ClampedThrottle = FMath::Clamp<float>(Throttle, -1, 1);
+
+	auto ForceApplied = GetForwardVector() * ClampedThrottle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+}
+
---
+++ Tank_Fite/Source/Tank_Fite/TankTrack.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to set maximum driving force, and to apply forces to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class TANK_FITE_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // assume 40 tonne tank and 1g acceleration
+	
+	
+};
