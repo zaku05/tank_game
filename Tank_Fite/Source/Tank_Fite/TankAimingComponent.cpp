22c5b112f0081bb9b71dfe1f7849c44fbb1e8926
FIVE-PC\five <0dd05yy@gmail.com>
2017-09-18 22:21:42 -0500

    188 -- stable commit

--- Tank_Fite/Source/Tank_Fite/TankAimingComponent.cpp
+++ Tank_Fite/Source/Tank_Fite/TankAimingComponent.cpp
@@ -102,7 +102,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
 
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if (DeltaRotator.Yaw < 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->TurretRotate(DeltaRotator.Yaw);
 	}
